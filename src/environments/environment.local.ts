5d381ef4e377753ab028c968b6e607d2aecca173
Simon Wiles <simonjwiles@gmail.com>
2017-09-28 10:02:51 -0700

    remove environment.local

--- src/environments/environment.local.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiURL: 'https://lr.simonwiles.net/api/',
-  baseURL: 'http://localhost/lr-build/',
-  baseHref: '/lr-build/',
-  mediaURL: 'https://lr.simonwiles.net/',
-  lagunitaHost: 'https://preview.lagunita.stanford.edu'
-};
