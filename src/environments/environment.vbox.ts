b216693d337924831fe9f3355129680f4b854e71
Simon Wiles <simonjwiles@gmail.com>
2018-09-11 17:35:35 -0700

    add an Angular environment and npm command for testing in a VirtualBox guest (i.e. OSX)

---
+++ src/environments/environment.vbox.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  hmr: false,
+  apiURL: 'https://10.0.2.2:8000/api/',
+  baseURL: '/',
+  baseHref: '/',
+  mediaURL: 'https://10.0.2.2:8000/media/',
+  lagunitaHost: 'https://lagunita.stanford.edu',
+  lagunitaPreviewHost: 'https://preview.lagunita.stanford.edu',
+
+  maxRecordingFileSize: 10e+6,
+  maxSupportingFileSize: 2e+6,
+  maxSupportingFileCount: 5
+};
